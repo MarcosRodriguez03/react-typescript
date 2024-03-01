14e3525305711319b3f7f1b22dc8e77d42965e17
MarcosRodriguez03 <marcos.e.rodriguez2003@gmail.com>
2024-02-29 21:34:56 -0800

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import getData from './DataServices/DataServices';
+import { userInfo } from 'os';
+import astronaut from './Interfaces/interfaces';
 
 function App() {
+  const [astroInfo, setAstroInfo] = useState<astronaut>();
+
+  useEffect(() => {
+    const astroData = async () => {
+      const fetchedData = await getData();
+      setAstroInfo(fetchedData);
+
+    }
+    astroData()
+
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{astroInfo?.people.map((person) => {
+        return person.name + " " + person.craft
+      })}</h1>
     </div>
   );
 }
